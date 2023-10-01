87ee1639c5fbaee25e4fe232b7402b368af2ee5d
Martin Roldan <gmroldanutn@gmail.com>
2023-10-01 09:46:04 +0300

    Fix cors issue

--- src/main/java/com/example/projecttrackingapi/controller/TaskController.java
+++ src/main/java/com/example/projecttrackingapi/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.example.projecttrackingapi.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,7 @@ public class TaskController {
     }
 
     @GetMapping("/tasks")
+    @CrossOrigin
     public ResponseEntity findAll(@RequestParam int page,
                                   @RequestParam int size) {
         var result = taskService.findAll(page, size);
