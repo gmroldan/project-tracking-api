a3a16ce18edcca5f30371629f60854bd54980711
Martin Roldan <gmroldanutn@gmail.com>
2023-10-01 10:14:10 +0300

    Add cors config

--- src/main/java/com/example/projecttrackingapi/controller/TaskController.java
+++ src/main/java/com/example/projecttrackingapi/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.example.projecttrackingapi.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 public class TaskController {
 
