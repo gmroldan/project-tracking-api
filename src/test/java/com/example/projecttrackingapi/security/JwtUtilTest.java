fcc8f98e189f32e1024fcdd2ca99f92a3fe9d751
Martin Roldan <gmroldanutn@gmail.com>
2024-09-16 22:51:03 +0300

    Fix unit tests execution

--- src/test/java/com/example/projecttrackingapi/security/JwtUtilTest.java
+++ src/test/java/com/example/projecttrackingapi/security/JwtUtilTest.java
@@ -2,12 +2,16 @@ package com.example.projecttrackingapi.security;
 
 import com.example.projecttrackingapi.dto.UserDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class JwtUtilTest {
 
     private JwtUtil jwtUtil;
@@ -17,6 +21,7 @@ class JwtUtilTest {
         this.jwtUtil = new JwtUtil("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=", 3600000L);
     }
 
+    @Order(1)
     @Test
     void createToken_whenUserHasJustLoggedIn_ReturnsNewToken() {
         final var userDto = new UserDto(1L, "hsimpson", "Homer", "Simpson");
@@ -27,6 +32,7 @@ class JwtUtilTest {
         assertTrue(jwtUtil.isTokenValid(token));
     }
 
+    @Order(2)
     @Test
     void invalidateToken_whenUserHasJustLoggedIn_addTokenToBlackList() {
         final var userDto = new UserDto(1L, "hsimpson", "Homer", "Simpson");
